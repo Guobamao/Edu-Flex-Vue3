ba0e97f436d8d70fc2d085a9243a4a67310ef671
Guobamao <1972212424@qq.com>
2024-10-06 17:14:40 +0800

    新增教师管理API与页面视图

---
+++ src/views/manage/teacher/index.vue
@@ -0,0 +1,284 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px" @submit.prevent>
+      <el-form-item label="所属学院" prop="collegeId">
+        <el-select v-model="queryParams.collegeId" placeholder="请选择所属学院" clearable style="width: 200px;"
+          @change="handleQuery" @clear="handleQuery">
+          <el-option v-for="item in collegeList" :key="item.id" :label="item.name" :value="item.id" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="教师姓名" prop="nickName">
+        <el-input v-model="queryParams.nickName" placeholder="请输入教师姓名" clearable @keyup.enter="handleQuery" />
+      </el-form-item>
+      <el-form-item label="手机号码" prop="phonenumber">
+        <el-input v-model="queryParams.phonenumber" placeholder="请输入手机号码" clearable @keyup.enter="handleQuery" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button type="primary" plain icon="Plus" @click="handleAdd"
+          v-hasPermi="['manage:teacher:add']">新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate"
+          v-hasPermi="['manage:teacher:edit']">修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete"
+          v-hasPermi="['manage:teacher:remove']">删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="warning" plain icon="Download" @click="handleExport"
+          v-hasPermi="['manage:teacher:export']">导出</el-button>
+      </el-col>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="teacherList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="序号" type="index" width="50" align="center" prop="id" />
+      <el-table-column label="教师名称" align="center" prop="nickName" />
+      <el-table-column label="手机号码" align="center" prop="phonenumber" />
+      <el-table-column label="所属学院" align="center" prop="collegeId">
+        <template #default="scope">
+          <div v-for="item in collegeList" :key="item.id" >
+            <span v-if="scope.row.collegeId === item.id">{{ item.name }}</span>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template #default="scope">
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
+            v-hasPermi="['manage:teacher:edit']">修改</el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
+            v-hasPermi="['manage:teacher:remove']">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum"
+      v-model:limit="queryParams.pageSize" @pagination="getList" />
+
+    <!-- 添加或修改教师管理对话框 -->
+    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+      <el-form ref="teacherRef" :model="form" :rules="rules" label-width="100px">
+        <el-form-item label="教师登录名" prop="userName">
+          <el-input v-model="form.userName" placeholder="请输入教师登录名" :disabled="form.userId"/>
+        </el-form-item>
+        <el-form-item label="密码" prop="password" v-if="!form.userId">
+          <el-input v-model="form.password" placeholder="请输入密码" type="password" show-password/>
+        </el-form-item>
+        <el-form-item label="教师姓名" prop="nickName">
+          <el-input v-model="form.nickName" placeholder="请输入教师姓名" />
+        </el-form-item>
+        <el-form-item label="所属学院" prop="collegeId">
+          <el-select v-model="form.collegeId" placeholder="请选择所属学院" clearable>
+            <el-option v-for="item in collegeList" :key="item.id" :label="item.name" :value="item.id" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="手机号码" prop="phonenumber">
+          <el-input v-model="form.phonenumber" placeholder="请输入手机号码" maxlength="11" />
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="form.email" placeholder="请输入邮箱" />
+        </el-form-item>
+        <el-form-item label="性别" prop="sex">
+          <el-select v-model="form.sex" placeholder="请选择性别">
+            <el-option v-for="dict in sys_user_sex" :key="dict.value" :label="dict.label" :value="dict.value" />
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup name="Teacher">
+import { listTeacher, getTeacher, delTeacher, addTeacher, updateTeacher } from "@/api/manage/teacher";
+import { listCollege } from '@/api/manage/college';
+import { loadAllParams } from '@/api/page';
+
+const { proxy } = getCurrentInstance();
+const { sys_user_sex } = proxy.useDict("sys_user_sex");
+
+const teacherList = ref([]);
+const open = ref(false);
+const loading = ref(true);
+const showSearch = ref(true);
+const ids = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    collegeId: null, // 所属学院ID
+    nickName: null, // 教师姓名
+    phonenumber: null, // 手机号
+  },
+  rules: {
+    collegeId: [
+      { required: true, message: "所属学院ID不能为空", trigger: "blur" }
+    ],
+    userName: [
+      { required: true, message: "用户名不能为空", trigger: "blur" }
+    ],
+    nickName: [
+      { required: true, message: "教师姓名不能为空", trigger: "blur" }
+    ],
+    email: [
+      { required: true, message: "邮箱地址不能为空", trigger: "blur" },
+      { type: "email", message: "请输入正确的邮箱地址", trigger: ["blur", "change"] }
+    ],
+    phonenumber: [
+      { required: true, message: "手机号码不能为空", trigger: "blur" },
+      { pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/, message: "请输入正确的手机号码", trigger: "blur" }
+    ],
+    password: [
+      { required: true, message: "密码不能为空", trigger: "blur" },
+      { min: 5, max: 20, message: "用户密码长度必须介于 5 和 20 之间", trigger: "blur" },
+      { pattern: /^[^<>"'|\\]+$/, message: "不能包含非法字符，默认：< > \" ' \\\ |", trigger: "blur" }
+    ]
+  }
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询教师管理列表 */
+function getList() {
+  loading.value = true;
+  listTeacher(queryParams.value).then(response => {
+    teacherList.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  });
+}
+
+// 取消按钮
+function cancel() {
+  open.value = false;
+  reset();
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    id: null,
+    userId: null,
+    collegeId: null,
+    userName: null,
+    nickName: null,
+    email: null,
+    phonenumber: null,
+    sex: null,
+    avatar: null,
+    password: null,
+    roleId: 3, // 默认教师角色
+  };
+  proxy.resetForm("teacherRef");
+}
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+/** 重置按钮操作 */
+function resetQuery() {
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+
+// 多选框选中数据
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.id);
+  single.value = selection.length != 1;
+  multiple.value = !selection.length;
+}
+
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加教师管理";
+}
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const _id = row.id || ids.value
+  getTeacher(_id).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改教师管理";
+  });
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["teacherRef"].validate(valid => {
+    if (valid) {
+      if (form.value.id != null) {
+        form.value.roleId = 3;
+        updateTeacher(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        });
+      } else {
+        addTeacher(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        });
+      }
+    }
+  });
+}
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const _ids = row.id || ids.value;
+  proxy.$modal.confirm('是否确认删除教师管理编号为"' + _ids + '"的数据项？').then(function () {
+    return delTeacher(_ids);
+  }).then(() => {
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch(() => { });
+}
+
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download('manage/teacher/export', {
+    ...queryParams.value
+  }, `teacher_${new Date().getTime()}.xlsx`)
+}
+
+/** 获取学院列表 */
+const collegeList = ref([]);
+
+function getCollegeList() {
+  listCollege(loadAllParams).then(response => {
+    // 过滤掉根节点
+    collegeList.value = response.data.filter(item => item.parentId !== 0);
+  });
+}
+
+getCollegeList();
+getList();
+</script>
