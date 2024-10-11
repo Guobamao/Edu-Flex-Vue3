b674c1765e7e5b657172d12f7957ba9888d07aae
Guobamao <1972212424@qq.com>
2024-10-11 15:55:44 +0800

    从课程搜索参数中移除 startTime 和 endTime
    在课程管理页面的搜索功能中，移除了开始时间和结束时间的搜索参数，以简化搜索表单并解决可能的性能问题。

---
+++ src/views/manage/course/index.vue
@@ -0,0 +1,296 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="课程名称" prop="name">
+        <el-input v-model="queryParams.name" placeholder="请输入课程名称" clearable @keyup.enter="handleQuery" />
+      </el-form-item>
+      <el-form-item label="时间范围">
+        <el-date-picker clearable v-model="dateRange" value-format="YYYY-MM-DD" type="daterange" range-separator="-"
+          start-placeholder="开始日期" end-placeholder="结束日期">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button type="primary" plain icon="Plus" @click="handleAdd" v-hasPermi="['manage:course:add']">新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate"
+          v-hasPermi="['manage:course:edit']">修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete"
+          v-hasPermi="['manage:course:remove']">删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="warning" plain icon="Download" @click="handleExport"
+          v-hasPermi="['manage:course:export']">导出</el-button>
+      </el-col>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="courseList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="序号" type="index" width="50" align="center" prop="id" />
+      <el-table-column label="课程名称" align="center" prop="name" />
+      <el-table-column label="任课老师" align="center" prop="teacherId">
+        <template #default="scope">
+          <div v-for="item in teacherList" :key="item.userId">
+            <span v-if="scope.row.teacherId === item.userId">{{ item.nickName }}</span>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="开始时间" align="center" prop="startTime" width="180">
+        <template #default="scope">
+          <span>{{ parseTime(scope.row.startTime, '{y}-{m}-{d}') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="结束时间" align="center" prop="endTime" width="180">
+        <template #default="scope">
+          <span>{{ parseTime(scope.row.endTime, '{y}-{m}-{d}') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="课程状态" align="center" prop="status">
+        <template #default="scope">
+          <dict-tag :options="course_status" :value="scope.row.status" />
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template #default="scope">
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
+            v-hasPermi="['manage:course:edit']">修改</el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
+            v-hasPermi="['manage:course:remove']">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum"
+      v-model:limit="queryParams.pageSize" @pagination="getList" />
+
+    <!-- 添加或修改课程管理对话框 -->
+    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+      <el-form ref="courseRef" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="课程名称" prop="name">
+          <el-input v-model="form.name" placeholder="请输入课程名称" />
+        </el-form-item>
+        <el-form-item label="任课老师" prop="teacherId">
+          <el-select v-model=form.teacherId placeholder="请选择任课老师" clearable>
+            <el-option v-for="item in teacherList" :key="item.userId" :label="item.nickName" :value="item.userId" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="开始时间" prop="startTime">
+          <el-date-picker clearable v-model="form.startTime" type="date" value-format="YYYY-MM-DD"
+            placeholder="请选择开始时间">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="结束时间" prop="endTime">
+          <el-date-picker clearable v-model="form.endTime" type="date" value-format="YYYY-MM-DD" placeholder="请选择结束时间">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="课程状态" prop="status">
+          <el-select v-model="form.status" placeholder="请选择课程状态" clearable>
+            <el-option v-for="dict in course_status" :key="dict.value" :label="dict.label" :value="dict.value" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="课程描述" prop="description">
+          <el-input v-model="form.description" type="textarea" placeholder="请输入内容" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup name="Course">
+import { listCourse, getCourse, delCourse, addCourse, updateCourse } from "@/api/manage/course";
+import { listTeacher } from "@/api/manage/teacher";
+import { loadAllParams } from '@/api/page';
+
+const { proxy } = getCurrentInstance();
+const { course_status } = proxy.useDict("course_status");
+
+const courseList = ref([]);
+const open = ref(false);
+const loading = ref(true);
+const showSearch = ref(true);
+const ids = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+
+const dateRange = ref([]);
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    name: null,
+    startTime: null,
+    endTime: null,
+    status: null,
+  },
+  rules: {
+    name: [
+      { required: true, message: "课程名称不能为空", trigger: "blur" }
+    ],
+    teacherId: [
+      { required: true, message: "任课老师不能为空", trigger: "blur" }
+    ],
+    startTime: [
+      { required: true, message: "开始时间不能为空", trigger: "blur" }
+    ],
+    endTime: [
+      { required: true, message: "结束时间不能为空", trigger: "blur" }
+    ],
+    status: [
+      { required: true, message: "课程状态不能为空", trigger: "change" }
+    ],
+  }
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询课程管理列表 */
+function getList() {
+  loading.value = true;
+  queryParams.value.params = {}
+  if (dateRange && dateRange.value) {
+    queryParams.value.params['startTime'] = dateRange.value[0];
+    queryParams.value.params['endTime'] = dateRange.value[1];
+  }
+  listCourse(queryParams.value).then(response => {
+    courseList.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  });
+}
+
+// 取消按钮
+function cancel() {
+  open.value = false;
+  reset();
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    id: null,
+    name: null,
+    description: null,
+    teacherId: null,
+    status: null,
+    createBy: null,
+    createTime: null,
+    updateBy: null,
+    updateTime: null
+  };
+  proxy.resetForm("courseRef");
+}
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+/** 重置按钮操作 */
+function resetQuery() {
+  dateRange.value = [];
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+
+// 多选框选中数据
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.id);
+  single.value = selection.length != 1;
+  multiple.value = !selection.length;
+}
+
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加课程管理";
+}
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const _id = row.id || ids.value
+  getCourse(_id).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改课程管理";
+  });
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["courseRef"].validate(valid => {
+    if (valid) {
+      if (form.value.id != null) {
+        updateCourse(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        });
+      } else {
+        addCourse(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        });
+      }
+    }
+  });
+}
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const _ids = row.id || ids.value;
+  proxy.$modal.confirm('是否确认删除课程编号为"' + _ids + '"的数据项？').then(function () {
+    return delCourse(_ids);
+  }).then(() => {
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch(() => { });
+}
+
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download('manage/course/export', {
+    ...queryParams.value
+  }, `course_${new Date().getTime()}.xlsx`)
+}
+
+const teacherList = ref([]);
+function getTeacherList() {
+  listTeacher(loadAllParams).then(resposne => {
+    teacherList.value = resposne.rows;
+  })
+}
+
+function handleDateChange(value) {
+  if (value) {
+    queryParams.value.startTime = value[0];
+    queryParams.value.endTime = value[1];
+  }
+}
+
+getTeacherList();
+getList();
+</script>
